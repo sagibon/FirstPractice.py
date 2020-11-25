376f9d04cba4b699b798e97bb28f8a641aeb9d2a
sagi bondarenko <sagibon@gmail.com>
2020-11-25 11:34:25 +0200

    Function roundd changed to rounddd

--- ThirdPackage/GUI.py
+++ ThirdPackage/GUI.py
@@ -1,22 +1,25 @@
-import tkinter,os
+import tkinter
 def click():
     output.delete(1.0,tkinter.END)
     text = entry1.get()
     encrypted = ''
     for i in text:
-        encrypted+=chr(ord(i)^11)
+        encrypted+=chr(ord(i)^123)
     output.insert(tkinter.INSERT, encrypted)
+    return encrypted
 def copy():
     r = tkinter.Tk()
     r.withdraw()
     r.clipboard_clear()
-    r.clipboard_append(output.get())
+    r.clipboard_append(click())
     r.update()  # now it stays on the clipboard after the window is closed
     r.destroy()
+def paste():
+    clipboard = window.clipboard_get()
+    entry1.insert(0, clipboard)
 
-def addToClipBoard(text):
-    command = 'echo ' + text.strip() + '| clip'
-    os.system(command)
+def clear():
+    entry1.delete(0, tkinter.END)
 
 #MAIN WINDOW
 window = tkinter.Tk()
@@ -32,8 +35,11 @@ entry1.grid(row = 1, column = 0, sticky='w')
 button1 = tkinter.Button(window, text = 'SUBMIT',command = click).grid(row = 1, column = 1, sticky='e')
 #copy button
 button2 = tkinter.Button(window, text = 'COPY', width = 6,command = copy).grid(row = 2, column = 1, sticky = 'e')
-#delete button
-#button3 = tkinter.Button(window, text = 'CLEAR',width = 6,command = clear).grid(row = 3, column = 1, sticky = 'e')
+#paste button
+button3 = tkinter.Button(window, text = 'PASTE', width = 6,command = paste).grid(row = 1, column = 2, sticky = 'e')
+#clear button
+button4 = tkinter.Button(window, text = 'CLEAR',width = 6,command = clear).grid(row = 2, column = 2, sticky = 'e')
+
 #another label
 label1 =tkinter.Label(window, text = "New text: ",bg = 'black', fg = 'white',font = "arial")
 label1.grid(row = 2, column = 0, sticky='w')
